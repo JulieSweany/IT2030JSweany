d65a532be9c3e6a7b6ee24000524a592c91d2559
JulieSweany <31965277+JulieSweany@users.noreply.github.com>
2019-01-24 10:40:13 -0500

    Completed lab 2

---
+++ Lab2JSweany/Lab2JSweany/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2JSweany.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public string Index()
+        {
+            return "Products/Index is displayed";
+        }
+
+        // GET: Products/Browse
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+
+        // GET: Products/Details/105
+        public string Details(int id)
+        {
+            string message = "Details displayed for Id=" + id;
+            return message;
+        }
+
+        // GET: Products/Location?zip=44124
+        public string Location(string zip)
+        {
+            string message = "Location displayed for zip=" + zip;
+            return message;
+        }
+    }
+}
---
+++ Lab2JSweany/Lab2JSweany/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab2JSweany.Startup))]
+namespace Lab2JSweany
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
