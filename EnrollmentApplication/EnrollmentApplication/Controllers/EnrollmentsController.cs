cd10572d6e2bbe77c816ab2d26fff39f19d2250f
JulieSweany <31965277+JulieSweany@users.noreply.github.com>
2019-02-15 16:51:39 -0500

    Completed lab 5

--- EnrollmentApplication/EnrollmentApplication/Controllers/EnrollmentsController.cs
+++ EnrollmentApplication/EnrollmentApplication/Controllers/EnrollmentsController.cs
@@ -39,8 +39,8 @@ namespace EnrollmentApplication.Controllers
         // GET: Enrollments/Create
         public ActionResult Create()
         {
-           // ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "CourseTitle");
-           // ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName");
+           ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "CourseTitle");
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName");
             return View();
         }
 
@@ -49,7 +49,7 @@ namespace EnrollmentApplication.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EnrollmentId,StudentId,CourseId,Grade")] Enrollment enrollment)
+        public ActionResult Create([Bind(Include = "EnrollmentId,StudentId,LastName,CourseId,Grade,IsActive,AssignedCampus,EnrollmentSemester,EnrollmentYear")] Enrollment enrollment)
         {
             if (ModelState.IsValid)
             {
