557eae7b5bb49d75a8a873e9207e0efa2ed7ad52
JulieSweany <31965277+JulieSweany@users.noreply.github.com>
2019-03-06 13:42:37 -0500

    Lab 8 completed

---
+++ Lab8_AuthWithGoogle/Lab8_AuthWithGoogle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab8_AuthWithGoogle.Startup))]
+namespace Lab8_AuthWithGoogle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
