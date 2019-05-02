90967f6444379d897f221c5769c5f2ee8acae881
JulieSweany <31965277+JulieSweany@users.noreply.github.com>
2019-05-02 09:20:09 -0400

    Backup before restarting work on Ordering (Had very basic shopping cart working)

---
+++ EventApplication/EventApplication/Models/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventApplication.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int RecordId { get; set; }
+
+        public string CartId { get; set; }
+
+        public int EventId { get; set; }
+
+        public virtual Event EventSelected { get; set; }
+
+        public int Count { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
